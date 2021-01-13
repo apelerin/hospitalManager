52c98acfd315472886e140b9deeb66acf64a3964
Axel Pélerin <arcanynka@gmail.com>
2021-01-13 10:33:37 +0100

    Fix while loop on main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String whatYouGonnaDo = "What do you wanna do?\n1. Access to hospital manager\n2. Play Colossal Cava\n3. Exit.";
-        System.out.println(whatYouGonnaDo);
-        int choice = sc.nextInt();
+        int choice;
         boolean chosen = false;
         while (!chosen) {
+            System.out.println(whatYouGonnaDo);
+            choice = sc.nextInt();
             switch (choice) {
                 case 1:
                     //code to go to the hospital manager
@@ -20,11 +21,10 @@ public class Main {
                     break;
                 case 3:
                     chosen = true;
+                    System.out.println("GoodBye.");
                     break;
                 default:
                     System.out.println("Choice is not available.");
-                    System.out.println(whatYouGonnaDo);
-                    choice = sc.nextInt();
                     break;
             }
         }

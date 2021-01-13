5e50559c223f2e0e8b08a17aa9efd7a166e0b059
Axel Pélerin <arcanynka@gmail.com>
2021-01-13 10:30:56 +0100

    Initial code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        String whatYouGonnaDo = "What do you wanna do?\n1. Access to hospital manager\n2. Play Colossal Cava\n3. Exit.";
+        System.out.println(whatYouGonnaDo);
+        int choice = sc.nextInt();
+        boolean chosen = false;
+        while (!chosen) {
+            switch (choice) {
+                case 1:
+                    //code to go to the hospital manager
+                    break;
+                case 2:
+                    //code to go to the game
+                    break;
+                case 3:
+                    chosen = true;
+                    break;
+                default:
+                    System.out.println("Choice is not available.");
+                    System.out.println(whatYouGonnaDo);
+                    choice = sc.nextInt();
+                    break;
+            }
+        }
     }
 }
